bbaf3ef9afffe88b598972df0d37c75c6e0936d5
asbam5 <asbam5@ulaval.ca>
2023-11-11 12:53:00 -0400

    correction encore pep8

--- phase1.py
+++ phase1.py
@@ -19,13 +19,13 @@ def analyser_commande():
     parser = argparse.ArgumentParser(
     description='Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.')
 
-    parser.add_argument('symbole', type=str,nargs='+', help="Nom d'un symbole boursier")
+    parser.add_argument('symbole', type=str,nargs='+',help="Nom d'un symbole boursier")
     parser.add_argument('-d','--début',metavar='DATE', type=str, 
     help='Date recherchée la plus ancienne (format: AAAA-MM-JJ)')
-    parser.add_argument('-f','--fin',metavar='DATE', type=str, 
+    parser.add_argument('-f','--fin',metavar='DATE', type=str,
     help='Date recherchée la plus récente (format: AAAA-MM-JJ)')
-    parser.add_argument('-v','--valeur',default='fermeture',type=str, 
-    choices=['fermeture', 'ouverture', 'min', 'max','volume'], 
+    parser.add_argument('-v','--valeur',default='fermeture',type=str,
+    choices=['fermeture', 'ouverture', 'min', 'max','volume'],
     help='La valeur désirée (par défaut: fermeture)')
     return parser.parse_args()
 
@@ -39,15 +39,13 @@ def produire_historique():
     for elemnt_symbole in get_parameters.symbole:
 
         url = f'https://pax.ulaval.ca/action/{elemnt_symbole}/historique/'
-        if get_parameters.début==None and get_parameters.fin!=None:
+        if get_parameters.début is None and get_parameters.fin is not None:
             get_parameters.début=get_parameters.fin
-        # check is date de fin n'existe pas il prend today 
-        if  get_parameters.début!=None and get_parameters.fin==None:
+        # check is date de fin n'existe pas il prend today
+        if  get_parameters.début is not None and get_parameters.fin is None:
             get_parameters.fin=str(datetime.date.today())
-        debut=datetime.datetime.strptime(get_parameters.début,'%Y-%m-%d')
-        fin=datetime.datetime.strptime(get_parameters.fin,'%Y-%m-%d')
-        list_date.append(debut.date())
-        list_date.append(fin.date())
+        list_date.append(datetime.datetime.strptime(get_parameters.début,'%Y-%m-%d').date())
+        list_date.append(datetime.datetime.strptime(get_parameters.fin,'%Y-%m-%d').date())
 
         params = {
             'début': get_parameters.début,
@@ -57,18 +55,14 @@ def produire_historique():
         réponse = json.loads(réponse.text)
         reponse_value=[]
         for key in réponse['historique'].keys():
-            key_date=datetime.datetime.strptime(key,'%Y-%m-%d')
-            reponse_value.append((key_date.date(),
+            reponse_value.append((datetime.datetime.strptime(key,'%Y-%m-%d').date(),
                                   réponse['historique'][key][get_parameters.valeur]))
-        date_debut=debut.date()
-        date_fin=fin.date()
         answer=""
         answer=f"titre={elemnt_symbole}: valeur={get_parameters.valeur}, début="
-        answer+=f"datetime.date({date_debut.year}, {date_debut.month}, {date_debut.day}), "
-        answer+=f"fin=datetime.date({date_fin.year}, {date_fin.month}, {date_fin.day})" 
+        answer+=f"datetime.date({list_date[0].year}, {list_date[0].month}, {list_date[0].day}), "
+        answer+=f"fin=datetime.date({list_date[1].year}, {list_date[1].month}, {list_date[1].day})"
         reponse_value.reverse()
-        reponse_fin=reponse_value
-        answer=answer+"\n"+str(reponse_fin)
+        answer=answer+"\n"+str(reponse_value)
         answer_out=answer_out+"\n"+answer
     return answer_out
 
